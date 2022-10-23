d8e4ce5e74a63349cdc2116b4caae5b5bbb786a2
chenghuan zhou <cheche282828@gmail.com>
2022-10-23 15:31:53 +0200

    create a sub directory for any language

---
+++ py/pre_verifica.py
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+import random
+
+def fill_list(p_list):
+    num = utilities.cinput("Quanti elementi vorresti generare? ",
+                            "Inserisci un numero intero: ",
+                            "Inserisci un numero positivo non nullo: ",
+                            int,
+                            lambda x: x > 0)
+
+    for i in range(num):
+        p_list.append(random.randint(0, 100))
+
+def split_list(p_list, condition):
+    return [x for x in p_list if condition(x)]
